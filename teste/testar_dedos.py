df39a06a2a298b6b4f322ece570f944ff59ed812
Joao Vitor <joaovitor.jaques.7748@gmail.com>
2025-08-07 16:48:38 -0300

    implementacao: implementando o teste do movimento de abrir e fechar os dedos

---
+++ teste/testar_dedos.py
@@ -0,0 +1,33 @@
+from arduino_servos import SetupArduino
+import time
+
+arduino = SetupArduino()
+
+
+def testar_todos():
+    """ Função para testar os dedos, primeiro inicializa todos em 0 e rotaciona em angulo ajustável com sleep() """
+
+    arduino.rotacionar_servo(arduino.pin1, 130)
+    arduino.rotacionar_servo(arduino.pin2, 130)
+    arduino.rotacionar_servo(arduino.pin3, 130)
+    arduino.rotacionar_servo(arduino.pin4, 130)
+
+    # pino 1 (indicador)
+    arduino.rotacionar_servo(arduino.pin1, 130)  # testando com 130, mas pode ser ajustado conforme necessidade
+    time.sleep(1)
+    arduino.rotacionar_servo(arduino.pin1, 130)
+
+    # pino 2 (médio)
+    arduino.rotacionar_servo(arduino.pin2, 130)
+    time.sleep(1)
+    arduino.rotacionar_servo(arduino.pin2, 0)
+
+    # pino 3 (anelar)
+    arduino.rotacionar_servo(arduino.pin3, 130)
+    time.sleep(1)
+    arduino.rotacionar_servo(arduino.pin3, 0)
+
+    # pino 4 (mindinho)
+    arduino.rotacionar_servo(arduino.pin4, 130)
+    time.sleep(1)
+    arduino.rotacionar_servo(arduino.pin4, 0)
